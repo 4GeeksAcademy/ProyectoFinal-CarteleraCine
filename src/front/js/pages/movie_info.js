3826afe6e833eb4289ec054b1c3eca233feaa18b
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-11-21 03:31:44 +0000

    working on displaying showtimes for each movie

--- src/front/js/pages/movie_info.js
+++ src/front/js/pages/movie_info.js
@@ -20,9 +20,9 @@ export const MovieInfo = () => {
             <div className="container">
                 <div className="row mt-5" key={movie.id}>
                     <div className="col-4">
-                        <img src={movie.image_url}  className="card-img-top" style={{width: "300px", height: "450px"}} alt="..."/>
+                        <img src={movie.image_url}  className="card-img-top mb-5" style={{width: "300px", height: "450px"}} alt="..."/>
                     </div>
-                    <div className="col-8 mt-5">
+                    <div className="col-8 mt-5 mb-5">
                         <h2>{movie.name}</h2>
                         <p>Fecha de estreno: {movie.release_date}</p>
                         <p className="fst-italic">Calificación: </p>
@@ -30,6 +30,75 @@ export const MovieInfo = () => {
                         <h5>Descripción</h5>
                         <p>{movie.overview}</p>
                     </div>
+                    <div className="">
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#paseo" aria-expanded="false" aria-controls="collapseExample">
+                                Cinépolis Paseo de las Flores Heredia
+                            </button>
+                        </div>
+                        <div className="collapse" id="paseo">
+                            <div className="card card-body d-grid gap-4 d-md-block col-12 mx-auto">
+                                <button className="btn btn-light">14:00</button>
+                                <button className="btn btn-light">16:00</button>
+                                <button className="btn btn-light">18:00</button>
+                                <button className="btn btn-light">20:00</button>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#oxigeno" aria-expanded="false" aria-controls="collapseExample">
+                                Cinemark Oxígeno Heredia
+                            </button>
+                        </div>
+                        <div className="collapse" id="oxigeno">
+                        <div className="card card-body d-grid gap-4 d-md-block col-12 mx-auto">
+                                <button className="btn btn-light">14:00</button>
+                                <button className="btn btn-light">16:00</button>
+                                <button className="btn btn-light">18:00</button>
+                                <button className="btn btn-light">20:00</button>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#molinos" aria-expanded="false" aria-controls="collapseExample">
+                                CineColombia Molinos Medellín
+                            </button>
+                        </div>
+                        <div className="collapse" id="molinos">
+                            <div className="card card-body d-grid gap-2 col-12 mx-auto">
+                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#santaFe" aria-expanded="false" aria-controls="collapseExample">
+                                CineColombia Santa Fe Medellín
+                            </button>
+                        </div>
+                        <div className="collapse" id="santaFe">
+                            <div className="card card-body d-grid gap-2 col-12 mx-auto">
+                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#florida" aria-expanded="false" aria-controls="collapseExample">
+                                Procinal Florida Medellín
+                            </button>
+                        </div>
+                        <div className="collapse" id="florida">
+                            <div className="card card-body d-grid gap-2 col-12 mx-auto">
+                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-info p-3 mb-1 d-grid gap-2 col-12 mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#aventura" aria-expanded="false" aria-controls="collapseExample">
+                                Procinal Aventura Medellín
+                            </button>
+                        </div>
+                        <div className="collapse mb-5" id="aventura">
+                            <div className="card card-body d-grid gap-2 col-12 mx-auto">
+                                Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
 			
